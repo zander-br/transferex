a7fbf7b0d15a6c0683d800fc003a1adbedb2b64a
Anderson Santos <anderson.feitoza.santos@gmail.com>
2022-02-02 20:24:06 -0300

    feat: when all params is valid and due_date is past, returns the transfer with status :rejected

--- lib/transferex/transfers/core/create_transfer.ex
+++ lib/transferex/transfers/core/create_transfer.ex
@@ -9,6 +9,16 @@ defmodule Transferex.Transfers.Core.CreateTransfer do
     |> Repo.insert()
   end
 
+  defp add_status_transfer(%{due_date: due_date} = transfer) do
+    now = Date.utc_today()
+    due_date = Date.from_iso8601!(due_date)
+
+    case Date.compare(due_date, now) do
+      :lt -> Map.put(transfer, :status, :rejected)
+      _ -> Map.put(transfer, :status, :created)
+    end
+  end
+
   defp add_status_transfer(transfer) do
     Map.put(transfer, :status, :created)
   end
