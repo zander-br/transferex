b2d1debf302f90c0c74936b0700d56754fb036a5
Anderson Santos <anderson.feitoza.santos@gmail.com>
2022-02-02 19:28:48 -0300

    refactor: remove due_date validation for changeset

--- test/transferex/transfers/data/transfer_test.exs
+++ test/transferex/transfers/data/transfer_test.exs
@@ -41,15 +41,6 @@ defmodule Transferex.Transfers.Data.TransferTest do
       assert errors_on(invalid_transfer) == expected_response
     end
 
-    test "when there is past due_date, return an error" do
-      invalid_attrs = build(:transfer_attrs, due_date: "2000-01-01")
-      expected_response = %{due_date: ["cannot be in past"]}
-
-      invalid_transfer = Transfer.changeset(invalid_attrs)
-
-      assert errors_on(invalid_transfer) == expected_response
-    end
-
     test "when invalid origin_account_id, returns an invalid changeset" do
       invalid_attrs = build(:transfer_attrs, %{origin_account_id: "invalid_id"})
 
