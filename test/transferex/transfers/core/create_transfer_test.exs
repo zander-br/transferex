1448124f0531362972d1f16ca340e3e304f646bb
Anderson Santos <anderson.feitoza.santos@gmail.com>
2022-02-02 20:24:29 -0300

    test: when all params is valid and due_date is past, returns the transfer with status :rejected

--- test/transferex/transfers/core/create_transfer_test.exs
+++ test/transferex/transfers/core/create_transfer_test.exs
@@ -12,5 +12,13 @@ defmodule Transferex.Transfers.Core.CreateTransferTest do
 
       assert {:ok, %Transfer{id: _id, inserted_at: _inserted_at, status: :created}} = response
     end
+
+    test "when all params is valid and due_date is past, returns the transfer with status :rejected" do
+      valid_transfer = build(:transfer_attrs, due_date: "2000-01-01")
+
+      response = CreateTransfer.execute(valid_transfer)
+
+      assert {:ok, %Transfer{id: _id, inserted_at: _inserted_at, status: :rejected}} = response
+    end
   end
 end
