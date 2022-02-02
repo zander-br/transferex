45caf2b22bb83afefd39e0e5262a0082545ebb71
Anderson Santos <anderson.feitoza.santos@gmail.com>
2022-02-02 14:44:16 -0300

    test: when there is past due_date, return an error

--- test/transferex/transfers/data/transfer_test.exs
+++ test/transferex/transfers/data/transfer_test.exs
@@ -39,5 +39,14 @@ defmodule Transferex.Transfers.Data.TransferTest do
 
       assert errors_on(invalid_transfer) == expected_response
     end
+
+    test "when there is past due_date, return an error" do
+      invalid_attrs = build(:transfer_attrs, due_date: "2000-01-01")
+      expected_response = %{due_date: ["cannot be in past"]}
+
+      invalid_transfer = Transfer.changeset(invalid_attrs)
+
+      assert errors_on(invalid_transfer) == expected_response
+    end
   end
 end
