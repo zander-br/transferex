46e9c3b2da3096d83ba862808b903613a5b82574
Anderson Santos <anderson.feitoza.santos@gmail.com>
2022-02-04 16:45:49 -0300

    feat: add Casex Plug in config

--- config/config.exs
+++ config/config.exs
@@ -27,6 +27,8 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :phoenix, :format_encoders, json: Casex.CamelCaseEncoder
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
