c46a8c3f724026d9f61f97c93d9abed783b6cfed
Anderson Santos <anderson.feitoza.santos@gmail.com>
2022-02-04 17:23:51 -0300

    feat: render error.json with changeset

--- lib/transferex_web/views/error_view.ex
+++ lib/transferex_web/views/error_view.ex
@@ -1,16 +1,27 @@
 defmodule TransferexWeb.ErrorView do
   use TransferexWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.html", _assigns) do
-  #   "Internal Server Error"
-  # end
+  alias Ecto.Changeset
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.html" becomes
-  # "Not Found".
-  def template_not_found(template, _assigns) do
-    Phoenix.Controller.status_message_from_template(template)
+  def render("error.json", %{changeset: %Changeset{errors: errors}}) do
+    errors =
+      Enum.map(errors, fn {field, detail} ->
+        %{
+          field: Inflex.camelize(field, :lower),
+          message: render_detail(detail)
+        }
+      end)
+
+    %{errors: errors}
+  end
+
+  defp render_detail({message, values}) do
+    Enum.reduce(values, message, fn {key, value}, acc ->
+      String.replace(acc, "%{#{key}}", to_string(value))
+    end)
+  end
+
+  defp render_detail(message) do
+    message
   end
 end
