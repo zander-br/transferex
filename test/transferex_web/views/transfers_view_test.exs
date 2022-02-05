1a499e3b9f18f17a7e47704c2e1209bd88d50cca
Anderson Santos <anderson.feitoza.santos@gmail.com>
2022-02-05 14:50:55 -0300

    test: render show.json view

--- test/transferex_web/views/transfers_view_test.exs
+++ test/transferex_web/views/transfers_view_test.exs
@@ -10,4 +10,20 @@ defmodule TransferexWeb.TransfersViewTest do
 
     assert %{id: transfer.id, status: transfer.status} == response
   end
+
+  test "renders show.json" do
+    transfer = build(:transfer)
+
+    response = render(TransfersView, "show.json", transfer: transfer)
+
+    assert %{
+             id: transfer.id,
+             status: transfer.status,
+             destination_account: transfer.destination_account_id,
+             due_date: transfer.due_date,
+             origin_account: transfer.origin_account_id,
+             amount: Decimal.new(100),
+             transfer_date: nil
+           } == response
+  end
 end
