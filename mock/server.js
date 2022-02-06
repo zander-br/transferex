c9a6e6e475175dbf6b6b3a20f977f42d7114465b
Anderson Santos <anderson.feitoza.santos@gmail.com>
2022-02-06 14:05:24 -0300

    chore: add generate error in mock liquidation api

--- mock/server.js
+++ mock/server.js
@@ -6,18 +6,37 @@ const port = process.env.PORT | 3333;
 
 app.use(express.json());
 
-app.post('/paymentOrders', (req, res) => {
-  console.log(req.body);
-
+app.post('/paymentOrders', (_req, res) => {
   const internalId = uuidv4();
   const status = ['APPROVED', 'REJECTED'];
   const randomStatus = status[Math.random() * status.length | 0];
+  const statusCode = getRandomStatusCode()
 
-  res.status(201).json({
+  if (statusCode !== 201) {
+    return res.status(statusCode).json({
+      message: 'simulated error'
+    });
+  }
+
+  return res.status(statusCode).json({
     internalId,
     status: randomStatus,
   });
 });
+
+function getRandomStatusCode() {
+  const randomInteger = getRandomInteger(1, 100);
+  const errorsStatusCode = [500, 405];
+  const randomErrorStatusCode = errorsStatusCode[Math.random() * errorsStatusCode.length | 0];
+
+  // Para simular um situação de erro de forma randomica
+  const isError = randomInteger % 3 === 0 && randomInteger % 5 === 0;
+  return isError ? randomErrorStatusCode : 201
+}
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 app.listen(port, () => {
   console.log(`API de liquidação fake rodando na porta ${port}`)
